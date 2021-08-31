382a706652ad1a95dcd33654f75bd6d47c74fc9d
HP <HP@Mahesh>
2021-08-31 12:07:33 +0530

    First Commit

---
+++ src/com/htc/demogit/Demo.java
@@ -0,0 +1,18 @@
+package com.htc.demogit;
+
+public class Demo {
+	public static void main(String[] args) {
+		
+	
+	int number = 4;
+	if(number%2==0)
+	{
+		System.out.println("Even number");
+	}
+	else
+	{
+		System.out.println("Odd number");
+	}
+}
+}
+
